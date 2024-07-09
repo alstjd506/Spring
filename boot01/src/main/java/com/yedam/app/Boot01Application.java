8f2680573107dc26cd8763d7bae9ad5f832efc6c
kimdogyeom <kdg412@naver.com>
2024-07-09 17:40:02 +0900

    스프링최초

---
+++ boot01/src/main/java/com/yedam/app/Boot01Application.java
@@ -0,0 +1,15 @@
+package com.yedam.app;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.yedam.app.**.mapper") //인터페이스를 쭈~욱 얽고 우리가 설정한 mapper location이 가진 쿼리를 쭈~~~욱 읽어서 합친다음 실행시키는
+public class Boot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot01Application.class, args);
+	}
+
+}
